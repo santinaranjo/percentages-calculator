42e5aea0b175e8b2643e9416cb9b7e73226c4065
santinaranjo <marketingsanti@gmail.com>
2021-12-09 11:50:52 +0100

    Lógica arreglada para devolver % total

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ function calculateDiscount() {
     const inputPriceValue = parseFloat(inputPrice.value);
     const inputDiscount = document.getElementById("discount__input");
     const inputDiscountValue = parseFloat(inputDiscount.value/100);
-    const result = (inputPriceValue * inputDiscountValue).toFixed(2);
+    const result = (inputPriceValue - (inputPriceValue * inputDiscountValue)).toFixed(2);
     const output = document.getElementById("discount__output").innerHTML = result;
     return output;
 }
